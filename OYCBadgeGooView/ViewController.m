4d33591f482662fbfe293059c39ea50f8b79d467
Ou Yangcheng <775350532@qq.com>
2016-12-13 14:39:23 +0800

    封装了一下，完善了一下代码

--- OYCBadgeGooView/ViewController.m
+++ OYCBadgeGooView/ViewController.m
@@ -19,11 +19,34 @@
     [super viewDidLoad];
 
     OYCBadgeGooView *badgeView = [OYCBadgeGooView badgeGooView];
-    badgeView.frame = CGRectMake(0, 0, 30, 30);
+    badgeView.frame = CGRectMake(0, 0, 20, 20);
+    badgeView.badgeNumber = 10;
+    badgeView.badgeColor = [UIColor cyanColor];
+    badgeView.badgeFontColor = [UIColor blackColor];
     badgeView.center = self.view.center;
+    [self.view addSubview:badgeView];
     
+    //创建消失时的动态图片
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 1; i < 9; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d",i]];
+        [array addObject:image];
+    }
+    
+    badgeView.boombImages = array;
+    
+}
+
+- (IBAction)add:(UIButton *)sender {
+    OYCBadgeGooView *badgeView = [OYCBadgeGooView badgeGooView];
+    badgeView.frame = CGRectMake(0, 0, 20, 20);
+    badgeView.badgeNumber = 10;
+    badgeView.badgeColor = [UIColor cyanColor];
+    badgeView.badgeFontColor = [UIColor blackColor];
+    badgeView.center = self.view.center;
     [self.view addSubview:badgeView];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
